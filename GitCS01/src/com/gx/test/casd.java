af3fd419be5a6a7d736ce4a63ab383b0af7fee3b
xiongjunjieCSDN <xjj1974788479@163.com>
2021-03-14 19:06:15 +0800

    wedas

--- GitCS01/src/com/gx/test/casd.java
+++ GitCS01/src/com/gx/test/casd.java
@@ -1,5 +1,29 @@
 package com.gx.test;
 
-public class casd {
+public class casd implements jk{
+
+	@Override
+	public void m() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String n() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void m01() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void m02() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
